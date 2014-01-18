938042e07ead20d6112222d20635f28bd69c177e
HUGE | Ian Bartholomew <ibartholomew@hugeinc.com>
2014-01-18 13:02:07 -0800

    adding vbo static cube

--- src/MeshTutorialApp.cpp
+++ src/MeshTutorialApp.cpp
@@ -2,6 +2,7 @@
 #include "cinder/gl/gl.h"
 #include "cinder/TriMesh.h"
 #include "cinder/MayaCamUI.h"
+#include "cinder/gl/Vbo.h"
 
 #include <vector>
 
@@ -18,7 +19,7 @@ public:
 	void update();
 	void draw();
     
-    ci::TriMesh mMesh;
+    gl::VboMeshRef          mVboMesh;
     
     // camera
 	MayaCamUI               mMayaCam;
@@ -29,56 +30,63 @@ void mMeshTutorialApp::setup()
 {
     // setup cameras
 	mCamera = CameraPersp(getWindowWidth(), getWindowHeight(), 75.0f, 50.0f, 10000.0f);
-	mCamera.setEyePoint( Vec3f(200.0f, 200.0f, 200.0f) );
-	mCamera.setCenterOfInterestPoint( Vec3f(0.0f, 0.0f, 0.0f) );
+	mCamera.setEyePoint( Vec3f(300.0f, 300.0f, 200.0f) );
+	mCamera.setCenterOfInterestPoint( Vec3f(100.0f, 100.0f, 0.0f) );
 	mMayaCam.setCurrentCam( mCamera );
     
-    // Create the points of our cube
-    Vec3f v0(-100,-100,-100);
-    Vec3f v1( 100,-100,-100);
-    Vec3f v2( 100, 100,-100);
-    Vec3f v3(-100, 100,-100);
-    Vec3f v4(-100,-100, 100);
-    Vec3f v5( 100,-100, 100);
-    Vec3f v6( 100, 100, 100);
-    Vec3f v7(-100, 100, 100);
+    gl::VboMesh::Layout layout;
+    layout.setStaticIndices();
+    layout.setDynamicColorsRGBA();
+    layout.setStaticPositions();
     
-    // Create the colors for each vertex
-    Color c0( 0, 0, 0 );
-    Color c1( 1, 0, 0 );
-    Color c2( 1, 1, 0 );
-    Color c3( 0, 1, 0 );
-    Color c4( 0, 0, 1 );
-    Color c5( 1, 0, 1 );
-    Color c6( 1, 1, 1 );
-    Color c7( 0, 1, 1 );
+    int vertCount = 24;
+    int quadCount = 6;
+    mVboMesh = gl::VboMesh::create(vertCount, quadCount * 4, layout, GL_QUADS);
     
-    Vec3f faces[6][4] = { /* Vertices for the 6 faces of a cube. */
-        {v0, v1, v2, v3}, {v3, v2, v6, v7}, {v7, v6, v5, v4},
-        {v4, v5, v1, v0}, {v5, v6, v2, v1}, {v7, v4, v0, v3} };
     
-    Color colors[6][4] = { /* colors for each vertex of the cube. */
-        {c0, c1, c2, c3}, {c3, c2, c6, c7}, {c7, c6, c5, c4},
-        {c4, c5, c1, c0}, {c5, c6, c2, c1}, {c7, c4, c0, c3} };
-    
-    for (int i = 0; i < 6; i++)
-    {
-        
-        mMesh.appendVertex(faces[i][0]);
-        mMesh.appendColorRgb(colors[i][0]);
-        mMesh.appendVertex(faces[i][1]);
-        mMesh.appendColorRgb(colors[i][1]);
-        mMesh.appendVertex(faces[i][2]);
-        mMesh.appendColorRgb(colors[i][2]);
-        mMesh.appendVertex(faces[i][3]);
-        mMesh.appendColorRgb(colors[i][3]);
-        
-        int numberVertices = mMesh.getNumVertices();
-        
-        mMesh.appendTriangle( numberVertices - 4, numberVertices - 3, numberVertices - 2 );
-        mMesh.appendTriangle( numberVertices - 4, numberVertices - 2, numberVertices - 1 );
-        
+    vector<uint32_t> indices;
+    int i=0;
+    while(i < 24){
+        indices.push_back(i);
+        i++;
     }
+    
+    mVboMesh->bufferIndices(indices);
+    
+    std::vector<Vec3f> positions;
+    
+    positions.push_back(Vec3f(100, 200, 1));
+    positions.push_back(Vec3f( 200, 200, 1));
+    positions.push_back(Vec3f( 200, 100, 1));
+    positions.push_back(Vec3f(100, 100, 1));
+    
+    positions.push_back(Vec3f( 200, 200, 1));
+    positions.push_back(Vec3f( 200, 200, 100));
+    positions.push_back(Vec3f( 200, 100, 100));
+    positions.push_back(Vec3f( 200, 100, 1));
+    
+    positions.push_back(Vec3f( 200, 200, 100));
+    positions.push_back(Vec3f(100, 200, 100));
+    positions.push_back(Vec3f(100, 100, 100));
+    positions.push_back(Vec3f( 200, 100, 100));
+    
+    positions.push_back(Vec3f(100, 200, 100));
+    positions.push_back(Vec3f(100, 200, 1));
+    positions.push_back(Vec3f(100, 100, 1));
+    positions.push_back(Vec3f(100, 100, 100));
+    
+    positions.push_back(Vec3f(100, 200, 100));
+    positions.push_back(Vec3f( 200, 200, 100));
+    positions.push_back(Vec3f( 200, 200, 1));
+    positions.push_back(Vec3f(100, 200, 1));
+    
+    positions.push_back(Vec3f(100, 100, 100));
+    positions.push_back(Vec3f( 200, 100, 100));
+    positions.push_back(Vec3f( 200, 100, 1));
+    positions.push_back(Vec3f(100, 100, 1));
+    
+    // now we can buffer positions
+    mVboMesh->bufferPositions(positions);
 }
 
 void mMeshTutorialApp::mouseDown( MouseEvent event )
@@ -106,50 +114,13 @@ void mMeshTutorialApp::update()
     // update the camera
     gl::setMatrices( mCamera );
     
-    if( mMesh.getNumVertices() == 0 ) return;
-    
-    // store all the mMesh information
-    
-    std::vector<Color> col = mMesh.getColorsRGB();
-    std::vector<Vec3f> vec = mMesh.getVertices();
-    
-    int i, j;
-    i = mMesh.getNumVertices();
-    j = 0;
-    
-    mMesh.clear();
-    
-    // something to add a little movement
-    float inc = sin( getElapsedSeconds() );
-    
-    while(j < i)
-    {
-        // alter the positions array to get a little dynamism
-        vec.at(j).x -= inc;
-        vec.at(j+1).x += inc;
-        vec.at(j+2).x += inc*2.0f;
-        vec.at(j+3).x -= inc*2.0f;
-        
-        // now replace it in the mMesh
-        mMesh.appendVertex( vec.at(j));
-        mMesh.appendColorRgb( col.at(j) );
-        mMesh.appendVertex( vec.at(j+1));
-        mMesh.appendColorRgb( col.at(j+1) );
-        mMesh.appendVertex( vec.at(j+2));
-        mMesh.appendColorRgb( col.at(j+2) );
-        mMesh.appendVertex( vec.at(j+3));
-        mMesh.appendColorRgb( col.at(j+3) );
-        
-        int vIdx0 = mMesh.getNumVertices() - 4;
-        int vIdx1 = mMesh.getNumVertices() - 3;
-        int vIdx2 = mMesh.getNumVertices() - 2;
-        int vIdx3 = mMesh.getNumVertices() - 1;
-        
-        mMesh.appendTriangle( vIdx0, vIdx1, vIdx2 );
-        mMesh.appendTriangle( vIdx0, vIdx2, vIdx3 );
-        
-        // go to the next triangle pair
-        j+=4;
+    float g = sin(getElapsedSeconds());
+    float b = cos(getElapsedSeconds());
+    gl::VboMesh::VertexIter iter = mVboMesh->mapVertexBuffer();
+    for( int x = 0; x < 24; ++x ) {
+        //positions.at(x) *= 1.001;
+        iter.setColorRGBA( ColorA( 1 - (g+b/3), g, b, 0.5) );
+        ++iter;
     }
     
     
@@ -162,7 +133,7 @@ void mMeshTutorialApp::draw()
 
     gl::enableWireframe();
     
-    gl::draw(mMesh);
+    gl::draw(mVboMesh);
     
 }
 
