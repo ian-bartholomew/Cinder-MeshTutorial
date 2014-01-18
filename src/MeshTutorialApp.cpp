a4835076d206d7f733b5e5137cd635d3a6ae9df2
HUGE | Ian Bartholomew <ibartholomew@hugeinc.com>
2014-01-18 13:45:43 -0800

    adding 2d map

--- src/MeshTutorialApp.cpp
+++ src/MeshTutorialApp.cpp
@@ -21,6 +21,8 @@ public:
     
     gl::VboMeshRef          mVboMesh;
     
+    static const int VERTICES_X = 50, VERTICES_Z = 50;
+    
     // camera
 	MayaCamUI               mMayaCam;
 	CameraPersp             mCamera;
@@ -29,64 +31,36 @@ public:
 void mMeshTutorialApp::setup()
 {
     // setup cameras
-	mCamera = CameraPersp(getWindowWidth(), getWindowHeight(), 75.0f, 50.0f, 10000.0f);
-	mCamera.setEyePoint( Vec3f(300.0f, 300.0f, 200.0f) );
-	mCamera.setCenterOfInterestPoint( Vec3f(100.0f, 100.0f, 0.0f) );
+	mCamera = CameraPersp(getWindowWidth(), getWindowHeight(), 75.0f, 200.0f, 10000.0f);
+	mCamera.setEyePoint( Vec3f(300.0f, 300.0f, 10.0f) );
+	mCamera.setCenterOfInterestPoint( Vec3f(0.0f, 0.0f, 0.0f) );
 	mMayaCam.setCurrentCam( mCamera );
     
-    gl::VboMesh::Layout layout;
-    layout.setStaticIndices();
-    layout.setDynamicColorsRGBA();
-    layout.setStaticPositions();
+	// setup the parameters of the Vbo
+	int totalVertices = VERTICES_X * VERTICES_Z;
+	int totalQuads = ( VERTICES_X - 1 ) * ( VERTICES_Z - 1 );
+	gl::VboMesh::Layout layout;
+	layout.setStaticIndices();
+	layout.setDynamicPositions();
+	layout.setStaticTexCoords2d();
+	mVboMesh = gl::VboMesh::create( totalVertices, totalQuads * 4, layout, GL_QUADS );
     
-    int vertCount = 24;
-    int quadCount = 6;
-    mVboMesh = gl::VboMesh::create(vertCount, quadCount * 4, layout, GL_QUADS);
-    
-    
-    vector<uint32_t> indices;
-    int i=0;
-    while(i < 24){
-        indices.push_back(i);
-        i++;
-    }
+	// buffer our static data - the texcoords and the indices
+	vector<uint32_t> indices;
+	for( int x = 0; x < VERTICES_X; ++x ) {
+		for( int z = 0; z < VERTICES_Z; ++z ) {
+			// create a quad for each vertex, except for along the bottom and right edges
+			if( ( x + 1 < VERTICES_X ) && ( z + 1 < VERTICES_Z ) ) {
+				indices.push_back( (x+0) * VERTICES_Z + (z+0) );
+				indices.push_back( (x+1) * VERTICES_Z + (z+0) );
+				indices.push_back( (x+1) * VERTICES_Z + (z+1) );
+				indices.push_back( (x+0) * VERTICES_Z + (z+1) );
+			}
+		}
+	}
     
     mVboMesh->bufferIndices(indices);
     
-    std::vector<Vec3f> positions;
-    
-    positions.push_back(Vec3f(100, 200, 1));
-    positions.push_back(Vec3f( 200, 200, 1));
-    positions.push_back(Vec3f( 200, 100, 1));
-    positions.push_back(Vec3f(100, 100, 1));
-    
-    positions.push_back(Vec3f( 200, 200, 1));
-    positions.push_back(Vec3f( 200, 200, 100));
-    positions.push_back(Vec3f( 200, 100, 100));
-    positions.push_back(Vec3f( 200, 100, 1));
-    
-    positions.push_back(Vec3f( 200, 200, 100));
-    positions.push_back(Vec3f(100, 200, 100));
-    positions.push_back(Vec3f(100, 100, 100));
-    positions.push_back(Vec3f( 200, 100, 100));
-    
-    positions.push_back(Vec3f(100, 200, 100));
-    positions.push_back(Vec3f(100, 200, 1));
-    positions.push_back(Vec3f(100, 100, 1));
-    positions.push_back(Vec3f(100, 100, 100));
-    
-    positions.push_back(Vec3f(100, 200, 100));
-    positions.push_back(Vec3f( 200, 200, 100));
-    positions.push_back(Vec3f( 200, 200, 1));
-    positions.push_back(Vec3f(100, 200, 1));
-    
-    positions.push_back(Vec3f(100, 100, 100));
-    positions.push_back(Vec3f( 200, 100, 100));
-    positions.push_back(Vec3f( 200, 100, 1));
-    positions.push_back(Vec3f(100, 100, 1));
-    
-    // now we can buffer positions
-    mVboMesh->bufferPositions(positions);
 }
 
 void mMeshTutorialApp::mouseDown( MouseEvent event )
@@ -103,7 +77,7 @@ void mMeshTutorialApp::mouseDrag( MouseEvent event )
 void mMeshTutorialApp::resize()
 {
     // now tell our Camera that the window aspect ratio has changed
-	mCamera.setPerspective( 75.0f, getWindowAspectRatio(), 1, 1000 );
+	mCamera.setPerspective( 75.0f, getWindowAspectRatio(), 300.0f, 1000 );
 	
 	// and in turn, let OpenGL know we have a new camera
 	gl::setMatrices( mCamera );
@@ -114,14 +88,21 @@ void mMeshTutorialApp::update()
     // update the camera
     gl::setMatrices( mCamera );
     
-    float g = sin(getElapsedSeconds());
-    float b = cos(getElapsedSeconds());
-    gl::VboMesh::VertexIter iter = mVboMesh->mapVertexBuffer();
-    for( int x = 0; x < 24; ++x ) {
-        //positions.at(x) *= 1.001;
-        iter.setColorRGBA( ColorA( 1 - (g+b/3), g, b, 0.5) );
-        ++iter;
-    }
+	const float timeFreq = 5.0f;
+	const float zFreq = 3.0f;
+	const float xFreq = 7.0f;
+	float offset = getElapsedSeconds() * timeFreq;
+    
+    
+	// dynmaically generate our new positions based on a simple sine wave
+	gl::VboMesh::VertexIter iter = mVboMesh->mapVertexBuffer();
+	for( int x = 0; x < VERTICES_X; ++x ) {
+		for( int z = 0; z < VERTICES_Z; ++z ) {
+			float height = sin( z / (float)VERTICES_Z * zFreq + x / (float)VERTICES_X * xFreq + offset ) / 5.0f;
+			iter.setPosition( Vec3f( x / (float)VERTICES_X, height, z / (float)VERTICES_Z ) );
+			++iter;
+		}
+	}
     
     
 }
@@ -131,6 +112,7 @@ void mMeshTutorialApp::draw()
 	// clear out the window with black
 	gl::clear( Color( 0, 0, 0 ) );
 
+    gl::scale( Vec3f( 200, 200, 200 ) );
     gl::enableWireframe();
     
     gl::draw(mVboMesh);
