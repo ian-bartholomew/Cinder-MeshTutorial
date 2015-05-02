333d5e79b02c3552fc33b82d77628142a00f5420
HUGE | Ian Bartholomew <ibartholomew@hugeinc.com>
2015-05-02 13:56:49 -0700

    adding cube

--- src/MeshTutorialApp.cpp
+++ src/MeshTutorialApp.cpp
@@ -19,25 +19,52 @@ public:
 void MeshTutorialApp::setup()
 {
         
-    // Position and Color for the four corners
-    mesh.appendVertex( Vec3f( 10, 10, 0 ) );
-    mesh.appendColorRgb( Color( 1, 0, 0 ) );
-    mesh.appendVertex( Vec3f( 10, 300, 0 ) );
-    mesh.appendColorRgb( Color( 0, 1, 0 ) );
-    mesh.appendVertex( Vec3f( 300, 300, 0 ) );
-    mesh.appendColorRgb( Color( 0, 0, 1 ) );
-    mesh.appendVertex( Vec3f( 300, 10, 0 ) );
-    mesh.appendColorRgb( Color( 0, 0, 0 ) );
+    // Create the points of our cube
+    Vec3f v0(-100,-100,-100);
+    Vec3f v1( 100,-100,-100);
+    Vec3f v2( 100, 100,-100);
+    Vec3f v3(-100, 100,-100);
+    Vec3f v4(-100,-100, 100);
+    Vec3f v5( 100,-100, 100);
+    Vec3f v6( 100, 100, 100);
+    Vec3f v7(-100, 100, 100);
     
-    // Indices for each of the four corners
-    int vIdx0 = mesh.getNumVertices() - 4;
-    int vIdx1 = mesh.getNumVertices() - 3;
-    int vIdx2 = mesh.getNumVertices() - 2;
-    int vIdx3 = mesh.getNumVertices() - 1;
+    // Create the colors for each vertex
+    Color c0( 0, 0, 0 );
+    Color c1( 1, 0, 0 );
+    Color c2( 1, 1, 0 );
+    Color c3( 0, 1, 0 );
+    Color c4( 0, 0, 1 );
+    Color c5( 1, 0, 1 );
+    Color c6( 1, 1, 1 );
+    Color c7( 0, 1, 1 );
     
-    // Two triangles to create our square
-    mesh.appendTriangle( vIdx0, vIdx1, vIdx2 );
-    mesh.appendTriangle( vIdx0, vIdx2, vIdx3 );
+    Vec3f faces[6][4] = { /* Vertices for the 6 faces of a cube. */
+        {v0, v1, v2, v3}, {v3, v2, v6, v7}, {v7, v6, v5, v4},
+        {v4, v5, v1, v0}, {v5, v6, v2, v1}, {v7, v4, v0, v3} };
+    
+    Color colors[6][4] = { /* colors for each vertex of the cube. */
+        {c0, c1, c2, c3}, {c3, c2, c6, c7}, {c7, c6, c5, c4},
+        {c4, c5, c1, c0}, {c5, c6, c2, c1}, {c7, c4, c0, c3} };
+    
+    for (int i = 0; i < 6; i++)
+    {
+        
+        mesh.appendVertex(faces[i][0]);
+        mesh.appendColorRgb(colors[i][0]);
+        mesh.appendVertex(faces[i][1]);
+        mesh.appendColorRgb(colors[i][1]);
+        mesh.appendVertex(faces[i][2]);
+        mesh.appendColorRgb(colors[i][2]);
+        mesh.appendVertex(faces[i][3]);
+        mesh.appendColorRgb(colors[i][3]);
+        
+        int numberVertices = mesh.getNumVertices();
+        
+        mesh.appendTriangle( numberVertices - 4, numberVertices - 3, numberVertices - 2 );
+        mesh.appendTriangle( numberVertices - 4, numberVertices - 2, numberVertices - 1 );
+        
+    }
 }
 
 void MeshTutorialApp::mouseDown( MouseEvent event )
